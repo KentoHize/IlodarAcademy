ab7f5e4578f0933464510298fafc1f680139efc9
日瀬 健人 <kentnewbie@hotmail.com>
2023-06-19 23:58:42 +0800

    T

---
+++ GraphicLibrary/Items/ArGeometricModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicLibrary.Items
+{
+    public class ArGeometricModel
+    {
+        ArFloatVector3[] vertices;
+        int[] indices;
+
+        public ArFloatVector3[] Vertices { get => vertices; set => vertices = value; }
+        public int[] Indices { get => indices; set => indices = value; }
+
+    }
+}
